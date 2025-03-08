b47067a04d49306eb68b4b6e74b3bdef259ceef1
Torey Lomenda <torey.lomenda@gmail.com>
2025-03-08 15:55:21 -0600

    Fix Flink SQL job to set 'payment' to null when performing left join where payment does not exist.

--- workspace/docker/flink-connectors/snack-orders-job.sql
+++ workspace/docker/flink-connectors/snack-orders-job.sql
@@ -24,11 +24,16 @@ SELECT
         s.customer_address,
         s.zip_code,
         ROW(s.latitude, s.longitude)) as shippingLocation,
-    ROW(
-        p.payment_type,
-        p.credit_card_type,
-        p.credit_card_number,
-        ROW(p.amount, IF(p.currency = 0, 'USD', 'USD'))) as payment
+    CASE
+        WHEN p.payment_type is null THEN
+            NULL
+        ELSE
+            ROW(
+                p.payment_type,
+                p.credit_card_type,
+                p.credit_card_number,
+                ROW(p.amount, IF(p.currency = 0, 'USD', 'USD')))
+    END as payment
 FROM item_details AS o
     INNER JOIN shipping_location AS s ON s.order_id = o.order_id
     LEFT JOIN payment AS p ON p.order_id = o.order_id
